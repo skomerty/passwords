fd3fcc7a392a01a66f8c5bad1d3bd9390947b42d
skomerty <75226554+skomerty@users.noreply.github.com>
2021-01-24 13:27:24 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+// Assignment code here
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+//ask for length
+var length = prompt("How many characters will your password be? Enter a number between 8 and 128");
+
+//ask for character type
+var charType = prompt("Enter a character type: special, numeric, uppercase, lowercase.");
+
+//generate password
+function generatePassword() {
+  //evaluate character type
+  var charSet = "";
+  var charTypeLower = charType.toLowerCase();
+  if( charTypeLower === "lowercase" ) {
+    charSet = "abcdefghijklmnopqrstuvwxyz";
+  } else if( charTypeLower === "uppercase" ) {
+    charSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  } else if( charTypeLower === "numeric" ) {
+    charSet = "0123456789";
+  } else if( charTypeLower === "special" ) {
+    charSet = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+  } 
+  //return value
+  var retVal = "";
+  for (var i = 0; i < length; i++) {
+    //picks a character within charSet at index of random number
+    retVal += charSet.charAt(Math.floor(Math.random() * charSet.length));
+  }
+  return retVal;
+}
